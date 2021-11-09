5548d052dc97e16bd8bd1f47bdb330a91270eaa9
t1k2a <maxtj1122@gmail.com>
2021-11-09 22:22:49 +0900

    エラー解消してブラッシュアップを図りたい

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, { useReducer, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../App.css';
 import Header from './Header';
 import Movie from './Movie';
 import Search from './Search';
 
 
-const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b";
+const MOVIE_API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=f588b052";
 
 const initialState = {
   loading: true,
@@ -16,46 +15,66 @@ const initialState = {
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "SEARCH_MOVIE_REQUEST":
+    case "SEARCH_MOVIES_REQUEST":
       return {
         ...state,
         loading: true,
         errorMessage: null
       };
-      
+    case "SEARCH_MOVIES_SUCCESS" :
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload
+      };
+    case "SEARCH_MOVIES_FAILURE" :
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.Error
+      };
+    default:
+      return state;
   }
-}
-function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const [errorMessage, setErroeMessage] = useState(null);
+};
 
-    useEffect(() => {
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  
+    useEffect (() => {
       fetch(MOVIE_API_URL)
         .then(response => response.json())
         .then(jsonResponse => {
-          setMovies(jsonResponse.Search);
-          setLoading(false)
+          dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonResponse.Search
+          });
         });
     }, []);
-
+    
     const search = searchValue => {
-      setLoading(true);
-      setErroeMessage(null);
-
-      fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      dispatch({
+        type: "SEARCH_MOVIES_REQUEST"
+      });
+      fetch(`https://www.omdbapi.com/?s=${searchValue}&i=tt3896198&apikey=f588b052`)
         .then(response => response.json())
         .then(jsonResponse => {
+          console.log(jsonResponse)
           if (jsonResponse.Response === "True") {
-            setMovies(jsonResponse.Search);
-            setLoading(false);
+            dispatch({
+              type: "SEARCH_MOVIES_SUCCESS",
+              payload: jsonResponse.Search
+            });
           } else {
-            setErroeMessage(jsonResponse.Error);
-            setLoading(false);
+            dispatch({
+              type: "SEARCH_MOVIES_FAILURE",
+              error: jsonResponse.Error
+            });
           }
         });
     };
-
+    
+    const { movies, errorMessage, loading } = state;
   return (
     <div className="App">
       <Header text="HOOKED" />
@@ -65,15 +84,13 @@ function App() {
         {loading && !errorMessage ? (
           <span>loading...</span>
         ) : errorMessage ? (
-          <div className="errorMEssage">{errorMessage}</div>
+          <div className="errorMessage">{errorMessage}</div>
         ) : (
-          movies.map((movie, index) => (
-            <Movie key={`${index}-${movie.Title}`} movie={movie} />
-          ))
+          movies 
         )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-// import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,5 +16,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
-// reportWebVitals();
-serviceWorker.unregister();
+reportWebVitals();
+// serviceWorker.unregister();
